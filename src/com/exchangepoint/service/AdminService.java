b5cc9a7d534eedd0f1b95baa6bd1e531f580c21e
YuliiaSydorenko <o972986990f@gmail.com>
2024-11-20 16:10:08 +0100

    admin

--- src/com/exchangepoint/service/AdminService.java
+++ src/com/exchangepoint/service/AdminService.java
@@ -1,9 +1,11 @@
 package com.exchangepoint.service;
 
-
+import com.exchangepoint.exception.AccountException;
+import com.exchangepoint.exception.AccountNotFoundException;
+import com.exchangepoint.exception.InsufficientFundsException;
+import com.exchangepoint.exception.UserNotFoundException;
 import com.exchangepoint.model.Account;
 import com.exchangepoint.model.Currency;
-import com.exchangepoint.exception.UserNotFoundException;
 import com.exchangepoint.model.Transaction;
 import com.exchangepoint.model.User;
 
@@ -16,4 +18,8 @@ public interface AdminService {
     List<User> getUsers();
     List<Transaction> getUserTransactions(Long userId);
     List<Account> getUserAccounts(Long userId);
+    void addUser(User user);
+    User findUserById(Long userId) throws UserNotFoundException;
+    void deleteUserById(Long userId) throws UserNotFoundException;
+    void updateUser(User user) throws UserNotFoundException;
 }
